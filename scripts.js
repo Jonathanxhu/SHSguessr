1c37efe52f91928542b424180031f417356349a6
Jonathanxhu <70634550+Jonathanxhu@users.noreply.github.com>
2023-06-04 13:50:06 +0800

    Update scripts.js
    Updated javascript code to accommodate new page layout; prevented multiple guesses

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ var currentLocation = null;
 var score = 0;
 var x = 0;
 var y = 0;
+var distance;
 var locations = [
     {src: 'https://drive.google.com/uc?export=view&id=1ZCHYpEnYeAnlQd39PQ3p2r2iF3cqWwPD', coordinates: {x: 694, y: 166}},
     {src: 'https://drive.google.com/uc?export=view&id=1jo1AW3jObbAywe5-mSj73idD51qbYvjh', coordinates: {x: 685, y: 472}},
@@ -9,7 +10,12 @@ var locations = [
 ];
 function getRandLocation(){
     currentLocation = locations[Math.floor(Math.random() * locations.length)];
-    return document.getElementById("result").innerHTML = '<img src="'+currentLocation.src+'" height="403" width="302">';    
+    document.getElementById("first").innerHTML = "";
+    distance = null;
+    document.getElementById("distance").textContent = distance;
+    document.getElementById("second").innerHTML = "";
+    document.getElementById("result").innerHTML = '<img src="'+currentLocation.src+'" height="403" width="302">';
+    return document.getElementById("guessbutton").innerHTML = '<input type="button" onclick="guess()" value="Guess!">';    
 }
 let container = document.getElementById("container");
 
@@ -21,14 +27,15 @@ container.addEventListener("click", function(event) {
     var dotx = x-5;
     var doty = y-5;
     dot.style.left = dotx + 'px';
-    dot.style.top = doty + 'px';
-    return document.getElementById("score").textContent = score;                
+    dot.style.top = doty + 'px';            
 });
 function guess(){
     var dx = x - currentLocation.coordinates.x;
     var dy = y - currentLocation.coordinates.y;    
-    var distance = Math.sqrt(dx*dx + dy*dy);
-    alert("Distance Away: " + distance);
+    distance = Math.sqrt(dx*dx + dy*dy);
+    document.getElementById("first").innerHTML = "Your guess is ";
+    document.getElementById("distance").textContent = distance;
+    document.getElementById("second").innerHTML = " away";
     score += (100 - distance);
     score = Math.floor(score);
     let dot = document.getElementById("dot");
@@ -36,5 +43,6 @@ function guess(){
     var doty = currentLocation.coordinates.y - 5
     dot.style.left = dotx + 'px';
     dot.style.top = doty + 'px';
-    return document.getElementById("score").textContent = score;
+    document.getElementById("score").textContent = score;
+    return document.getElementById("guessbutton").innerHTML = "";
 }
