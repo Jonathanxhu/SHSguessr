4feb382db1bc6db9d3adebd41867efd46cc032c6
Jonathanxhu <70634550+Jonathanxhu@users.noreply.github.com>
2023-06-04 17:09:25 +0800

    Create locations.js

---
+++ locations.js
@@ -0,0 +1,11 @@
+var locations = [
+    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1304.jpg', coordinates: {x: 694, y: 166}},
+    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1332.jpg', coordinates: {x: 685, y: 472}},
+    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1358.jpg', coordinates: {x: 954, y: 341}},
+    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1319.jpeg', coordinates: {x: 434, y: 251}},
+    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1321.jpeg', coordinates: {x: 365, y: 408}},
+    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1344.jpeg', coordinates: {x: 849, y: 582}},
+    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1357.jpeg', coordinates: {x: 959, y: 364}},
+    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1360.jpeg', coordinates: {x: 937, y: 291}},
+    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1323.jpeg', coordinates: {x: 481, y: 363}},
+];
