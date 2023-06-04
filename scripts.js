c7f2d813705d324a58255eaab7c91c9d126e2867
Jonathanxhu <70634550+Jonathanxhu@users.noreply.github.com>
2023-06-04 17:11:28 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,17 +3,6 @@ var score = 0;
 var x = 0;
 var y = 0;
 var distance;
-var locations = [
-    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1304.jpg', coordinates: {x: 694, y: 166}},
-    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1332.jpg', coordinates: {x: 685, y: 472}},
-    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1358.jpg', coordinates: {x: 954, y: 341}},
-    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1319.jpeg', coordinates: {x: 434, y: 251}},
-    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1321.jpeg', coordinates: {x: 365, y: 408}},
-    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1344.jpeg', coordinates: {x: 849, y: 582}},
-    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1357.jpeg', coordinates: {x: 959, y: 364}},
-    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1360.jpeg', coordinates: {x: 937, y: 291}},
-    {src: 'https://raw.githubusercontent.com/Jonathanxhu/SHSguessr/main/images/IMG_1323.jpeg', coordinates: {x: 481, y: 363}},
-];
 var indexes = [];
 var i = 0;
 while(i < locations.length){
